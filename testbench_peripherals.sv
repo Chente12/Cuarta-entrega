f03383585217a153fb5448c9b9fe6f8993a67871
Chente12 <vicente.zabaleta@udea.edu.co>
2026-06-15 21:09:06 -0500

    Create testbench_peripherals.sv

---
+++ testbench_peripherals.sv
@@ -0,0 +1,28 @@
+module testbench_peripherals();
+	logic clk;
+	logic reset;
+	logic [9:0] switches, leds;
+
+	localparam DELAY = 10;
+	
+	// instantiate device to be tested
+	top dut(clk, reset, switches, leds);
+
+	// initialize test
+	initial
+	begin
+		reset <= 0; #(DELAY*9.5); 
+		reset <= 1; 
+		
+		switches <= 10'd4; #(DELAY*1000);
+		
+		$stop;
+	end
+
+	// generate clock to sequence tests
+	always
+	begin
+		clk <= 1; #(DELAY/2); 
+		clk <= 0; #(DELAY/2);
+	end
+endmodule
